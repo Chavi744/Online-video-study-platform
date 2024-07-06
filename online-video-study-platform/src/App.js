95232c0447af1151e29ee9ce6bdf34af4b71033f
Avital Itach <137866503+AvitalItach@users.noreply.github.com>
2024-07-06 23:30:30 +0300

    Confirmation and completion screen

--- online-video-study-platform/src/App.js
+++ online-video-study-platform/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import ConfirmationScreen from './components/RegistrationConfirmation/Confirmation';
+import logo from './logo1.png';
 
 function App() {
+  // const [studentName, setStudentName] = useState('John Doe');
+  // const [selectedCourses, setSelectedCourses] = useState(['Math', 'Science']);
+  // const [totalCost, setTotalCost] = useState(300);
+  const [studentName, setStudentName] = useState('');
+  const [selectedCourses, setSelectedCourses] = useState([]);
+  const [totalCost, setTotalCost] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello world
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} alt="Logo" className="login-logo" />
+        </header>
+
+        <main>
+          <ConfirmationScreen 
+              studentName={studentName} 
+              selectedCourses={selectedCourses} 
+              totalCost={totalCost} 
+          />
+        </main>
+      </div>
   );
 }
 
